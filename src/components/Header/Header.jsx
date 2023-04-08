def819768cf17744af15944250467891b1ddac78
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-08 14:03:07 +0300

    Header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,2 +1,19 @@
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "redux/auth/authSelectors";
+import AuthNav from 'components/AuthNav/AuthNav';
+import UserMenu from 'components/UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+
+export const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <div>
+      <NavLink> Home </NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </div>
+  );
+};
+
+export default Header;
