cda83e24fb36229a74885905aa0a53d03f817446
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-08 08:25:20 +0300

    Index.js refactored

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import './index.css';
 import { ThemeProvider } from 'styled-components';
 
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = {
   colors: {
@@ -32,7 +34,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
