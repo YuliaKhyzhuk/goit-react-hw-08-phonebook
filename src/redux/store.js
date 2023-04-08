931ebf95dc7107dd5eb6a63545dcdbb0947ed577
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-08 08:06:13 +0300

    Store refactored

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+// import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/authSlice';
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: GetDefaultMiddleware => 
+  GetDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
