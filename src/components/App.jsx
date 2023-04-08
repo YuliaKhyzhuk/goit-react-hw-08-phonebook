312cd3c8566589a6415961ff0f6839f1b55a915f
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-08 10:12:10 +0300

    Restricted and Private routes added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { RestrictedRoute } from 'RestrictedRoute';
+import { PrivateRoute } from 'PrivateRoute';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
 import { selectError, selectIsLoading } from 'redux/selectors';
+
+import HomePage from 'Pages/HomePage';
 
 import Section from './Section/Section';
 import Form from './Form/Form';
 import Filter from './Filter/Filter';
 import ContactsList from './ContactsList/ContactsList';
 import Loader from './Loader/Loader';
+import Layout from './Layout/Layout';
+import Contacts from './Contacts/Contacts';
+
+import { fetchCurrentUser } from 'redux/auth/authOperations';
+import { selectIsRefreshing } from 'redux/auth/authSelectors';
 
 export function App() {
   const dispatch = useDispatch();
